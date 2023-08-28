a4eaca1968d0171f5f1a2abb7b26e01c5bb6ac36
Spencer C. Imbleau <spencer@imbleau.com>
2023-08-28 16:38:40 -0400

    module inception and todos

---
+++ src/parser/schema/animated_properties/mod.rs
@@ -0,0 +1,17 @@
+#[allow(clippy::module_inception)]
+mod animated_properties;
+pub use animated_properties::*;
+
+//todo pub mod animated_property;
+//todo pub mod color_value;
+//todo pub mod shape_keyframe;
+//todo pub mod multi_dimensional;
+//todo pub mod keyframe_base;
+//todo pub mod shape_property;
+//todo pub mod keyframe;
+//todo pub mod position;
+//todo pub mod keyframe_bezier_handle;
+//todo pub mod gradient_colors;
+//todo pub mod split_vector;
+//todo pub mod value;
+//todo pub mod position_keyframe;
