1aa285116a55ea794898801c5119fcaf30690cfb
Spencer C. Imbleau <spencer@imbleau.com>
2023-08-30 01:16:53 -0400

    corrected style type bounds

--- src/parser/schema/styles/layer_style.rs
+++ src/parser/schema/styles/layer_style.rs
@@ -1,9 +1,27 @@
 use crate::parser::{
-    breadcrumb::Breadcrumb, schema::helpers::visual_object::VisualObject,
-    util::MapExt, Error,
+    breadcrumb::{Breadcrumb, ValueType},
+    schema::helpers::visual_object::VisualObject,
+    util::MapExt,
+    Error,
 };
 use serde::{Deserialize, Serialize};
-use serde_json::{Number, Value};
+use serde_json::Value;
+use serde_repr::{Deserialize_repr, Serialize_repr};
+
+/// Style Type
+#[repr(u8)]
+#[derive(Deserialize_repr, Serialize_repr, Debug, Clone, PartialEq)]
+pub enum StyleType {
+    Stroke = 0,
+    DropShadow = 1,
+    InnerShadow = 2,
+    OuterGlow = 3,
+    InnerGlow = 4,
+    BevelEmboss = 5,
+    Satin = 6,
+    ColorOverlay = 7,
+    GradientOverlay = 8,
+}
 
 /// Style applied to a layer
 #[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
@@ -12,7 +30,7 @@ pub struct LayerStyle {
     pub visual_object: VisualObject,
     /// Style Type
     #[serde(rename = "ty")]
-    pub style_type: Number, // todo - this should be an enum but idk the valid range of integers
+    pub style_type: StyleType,
 }
 
 impl LayerStyle {
@@ -21,7 +39,8 @@ impl LayerStyle {
         obj: &serde_json::map::Map<String, Value>,
     ) -> Result<Self, Error> {
         let visual_object = VisualObject::from_obj(breadcrumb, obj);
-        let style_type = obj.extract_number(breadcrumb, "ty")?;
+        let style_type =
+            obj.extract_type(breadcrumb, "ty", ValueType::EnumInt)?;
         Ok(LayerStyle {
             visual_object,
             style_type,
